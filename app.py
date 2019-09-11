65a382a752eec7412f506f8b1f0be92065eea691
Chris Williams <cwill833@gmail.com>
2019-09-11 17:32:04 -0500

    add show one from books

--- app.py
+++ app.py
@@ -2,18 +2,20 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 books = [
     {
-        'name': 'The Cat In The Hat',
-        'price': 7.99,
-        'isbn': 34567890987654
+        "isbn": 34567890987654,
+        "name": "The Cat In The Hat",
+        "price": 7.99
     },
     {
-        'name': 'Green Eggs and Ham',
-        'price': 6.99,
-        'isbn': 98765434567803
+        "isbn": 98765434567803,
+        "name": "Green Eggs and Ham",
+        "price": 6.99
     }
 ]
+
 
 @app.route('/')
 def hello_world():
