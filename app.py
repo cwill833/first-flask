c7f4eebfd15c8b04f1aa469ed3b541a53195a090
Chris Williams <cwill833@gmail.com>
2019-09-11 16:17:29 -0500

    add GET to grab a specific element from the books list

--- app.py
+++ app.py
@@ -24,4 +24,16 @@ def hello_world():
 def show_books():
     return jsonify({'books': books })
 
+# GET /books/isbn
+@app.route('/books/<int:isbn>')
+def get_book_by_isbn(isbn):
+    return_value = {}
+    for book in books:
+        if book['isbn'] == isbn:
+            return_value = {
+                'name': book['name'],
+                'price': book['price']
+            }
+    return jsonify(return_value)
+
 app.run(port=5000)
