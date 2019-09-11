b7085ca656761dea7fe48ae3c5d2c75c62d6ab85
Chris Williams <cwill833@gmail.com>
2019-09-11 16:06:00 -0500

    add GET request to return books list

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-print(__name__)
+books = [
+    {
+        'name': 'The Cat In The Hat',
+        'price': 7.99,
+        'isbn': 34567890987654
+    },
+    {
+        'name': 'Green Eggs and Ham',
+        'price': 6.99,
+        'isbn': 98765434567803
+    }
+]
 
 @app.route('/')
 def hello_world():
     return "Hello World!"
+
+# GET /books
+@app.route('/books')
+def show_books():
+    return jsonify({'books': books })
 
 app.run(port=5000)
