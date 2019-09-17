b4ba0da92e42c1948d7370c2f46c83ec411be08f
Chris Williams <cwill833@gmail.com>
2019-09-17 14:17:14 -0500

    fix path for database uri

--- settings.py
+++ settings.py
@@ -2,5 +2,5 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATA_URI'] = 'sqllite:///Users/chriswilliams/Desktop/Dev/flask/first_flask/database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/chriswilliams/Desktop/Dev/flask/first_flask/database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
