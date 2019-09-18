395c10e37ed489cd3f7141931de7209a6d96e2ac
Chris Williams <cwill833@gmail.com>
2019-09-18 13:55:13 -0500

    add post funtion, get all function, get one function

--- BookModel.py
+++ BookModel.py
@@ -11,3 +11,22 @@ class Book(db.Model):
     name = db.Column(db.String(80), nullable=False)
     price = db.Column(db.Float, nullable=False)
     isbn = db.Column(db.Integer)
+
+    def add_book(_name, _price, _isbn):
+        new_book = Book(name = _name, price = _price, isbn = _isbn)
+        db.session.add(new_book)
+        db.session.commit()
+
+    def get_all_books():
+        return Book.query.all()
+    
+    def get_book(_isbn):
+        return Book.query.filter_by(isbn=_isbn).first()
+
+    def __repr__(self):
+        book_object = {
+            "name": self.name,
+            "price": self.price,
+            "isbn": self.isbn
+        }
+        return json.dumps(book_object)
