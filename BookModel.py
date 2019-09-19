711af18f3dbee9260c285a7c56e379f3583718d2
Chris Williams <cwill833@gmail.com>
2019-09-18 22:16:00 -0500

    add delete and update features

--- BookModel.py
+++ BookModel.py
@@ -12,6 +12,9 @@ class Book(db.Model):
     price = db.Column(db.Float, nullable=False)
     isbn = db.Column(db.Integer)
 
+    def json(self):
+        return {'name': self.name, 'price':self.price, 'isbn':self.isbn}
+
     def add_book(_name, _price, _isbn):
         new_book = Book(name = _name, price = _price, isbn = _isbn)
         db.session.add(new_book)
@@ -22,6 +25,27 @@ class Book(db.Model):
     
     def get_book(_isbn):
         return Book.query.filter_by(isbn=_isbn).first()
+
+    def delete_book(_isbn):
+        Book.query.filter_by(isbn=_isbn).delete()
+        db.session.commit()
+
+    def update_book_price(_isbn, _price):
+        book_to_update = Book.query.filter_by(isbn=_isbn).first()
+        book_to_update.price = _price
+        db.session.commit()
+
+    def update_book_name(_isbn, _name):
+        book_to_update = Book.query.filter_by(isbn=_isbn).first()
+        book_to_update.name = _name
+        db.session.commit()
+    
+    def replace_book(_isbn, _name, _price):
+        book_to_replace = Book.query.filter_by(isbn=_isbn).first()
+        book_to_replace.price = _price
+        book_to_replace.name = _name
+        db.session.commit()
+
 
     def __repr__(self):
         book_object = {
